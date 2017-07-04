2898e7759d5f50da8ea863eac24a54a36643537e
jushe <792988035@qq.com>
2017-07-04 15:29:28 +0800

    增加了抽屉栏

--- app/src/main/java/bupt/androidsipchat/MainActivity.java
+++ app/src/main/java/bupt/androidsipchat/MainActivity.java
@@ -1,104 +1,45 @@
 package bupt.androidsipchat;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by sheju on 2017/7/4.
+ */
 
 public class MainActivity extends AppCompatActivity {
 
-	boolean isLoginFront = true;
-	CardView login_card;
-	CardView register_card;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_icons8_menu);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.drawer_string, R.string.drawer_string);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.main_nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
 
 
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.launch_login_register);
-
-		if (savedInstanceState == null) {
-			getFragmentManager()
-					.beginTransaction()
-					.add(R.id.lanuch_login_register, new CardFrontFragment())
-					.commit();
-		}
-
-
-		//	setCameraDistance();
-		//initButton();
-	}
-
-//	private void initButton(){
-//		loginRegister.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				flipCard();
-//			}
-//		});
-//
-//		registerLogin.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				flipCard();
-//			}
-//		});
-//	}
-
-	private void setCameraDistance() {
-		login_card = (CardView) findViewById(R.id.login_card);
-		register_card = (CardView) findViewById(R.id.register_card);
-		int distance = 16000;
-		float scale = getResources().getDisplayMetrics().density * distance;
-		if (login_card != null) {
-			login_card.setCameraDistance(scale);
-		}
-		if (register_card != null) {
-			register_card.setCameraDistance(scale);
-		}
-	}
-
-	public static class CardFrontFragment extends android.app.Fragment {
-		@Nullable
-		@Override
-		public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-			return inflater.inflate(R.layout.card_login, container, false);
-		}
-	}
-
-	public static class CardBackFragment extends android.app.Fragment {
-		@Nullable
-		@Override
-		public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-			return inflater.inflate(R.layout.card_register, container, false);
-		}
-	}
-
-	public void flipCard(View view) {
-		if (!isLoginFront) {
-			getFragmentManager().popBackStack();
-			isLoginFront = true;
-			return;
-		}
-		isLoginFront = false;
-		android.app.FragmentTransaction ft = getFragmentManager()
-				.beginTransaction()
-				.setCustomAnimations(
-						R.animator.anim_right_in, R.animator.anim_right_out,
-						R.animator.anim_left_in, R.animator.anim_left_out)
-				.replace(R.id.lanuch_login_register, new CardBackFragment());
-		setCameraDistance();
-		ft.commit();
-
-
-	}
-
-
-
-
-
+    }
 }
